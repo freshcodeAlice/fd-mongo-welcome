75089c98c236a31a3e1c0f824fad0adb5f6d9a95
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-24 16:58:03 +0200

    populate + crud

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const postSchema = new Schema({
+    body: String,
+    imgSrc: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
